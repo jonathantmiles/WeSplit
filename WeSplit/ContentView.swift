958901e0f1d76ed5cde25a1926fd9f8f73c8ca23
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-10-22 23:52:49 -0700

    completed WeSplit project with challenges tweaks

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage]) // gets the value out of the array using the indexing
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        return orderAmount + tipValue
+    }
+    
+    var totalPerPerson: Double {
+        let numberOfPeopleAsADouble = Double(numberOfPeople) ?? 0
+//        let peopleCount = Double(numberOfPeopleAsADouble + 2) // the @State stored var, plus the offset
+        let amountPerPerson = grandTotal / numberOfPeopleAsADouble // peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.numberPad)
+//                    Picker("Number of People", selection: $numberOfPeople) {
+//                        ForEach (2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                }
+                Section(header: Text("How mch tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Total including tip")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }
     }
 }
 
